f0ff1c282d292158f26804c44ca829c223c37f98
神楽坂ニャン <lenvisual@gmail.com>
2020-05-12 02:16:45 +0800

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Lensual/IntelARK
 
 go 1.14
+
+require github.com/jinzhu/gorm v1.9.12
--- main.go
+++ main.go
@@ -4,11 +4,70 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/Lensual/IntelARK/odata"
+	"github.com/jinzhu/gorm"
+
+	"io/ioutil"
+
+	"encoding/base64"
+
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+type public_mobile_data struct {
+	D D `json:"d"`
+}
+
+type D struct {
+	Language string `json:"language"`
+	Checksum string `json:"checksum"`
+	DataBin  string `json:"data_bin"`
+}
+
 func main() {
+	language := "zh-cn"
+	timestamp := fmt.Sprintf("%v", time.Now().UnixNano())
+
+	res, err := Get("https://odata.intel.com/API/v1_0/Products/public_mobile_data('"+language+"')?"+
+		"format=json&"+timestamp, nil, nil)
+	if err != nil {
+		return
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	pmd := public_mobile_data{}
+	err = json.Unmarshal(data, &pmd)
+	if err != nil {
+		return
+	}
+
+	bin, err := base64.StdEncoding.DecodeString(pmd.D.DataBin)
+	if err != nil {
+		return
+	}
+
+	filename := "./" + pmd.D.Checksum + ".sqlite"
+
+	err = ioutil.WriteFile(filename, bin, os.ModePerm)
+	if err != nil {
+		return
+	}
+
+	db, err := gorm.Open("sqlite3", filename)
+	defer db.Close()
+	if err != nil {
+		return
+	}
+}
+
+func edmx() {
 	edmx, err := odata.QueryEdmx("https://odata.intel.com/API/v1_0/Products/$metadata")
 	if err != nil {
 		fmt.Printf("%+v", err)
